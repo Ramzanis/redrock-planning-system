3e164ac84f625a9e52d93614d7c24ac8105bf75f
MrTiberiu <39731777+MrTiberiu@users.noreply.github.com>
2022-03-02 16:36:00 +0100

    rer
    d

--- redrockback/backend/redrock/serializers.py
+++ redrockback/backend/redrock/serializers.py
@@ -17,8 +17,4 @@ class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subtask
         fields = ['subtaskID', 'operationID','containerID', 
-        'containerWeightT','loadSeq','moveTo','stow']<<<<<<< HEAD
         'containerWeightT','loadSeq','moveTo','stow']
-=======
-        'containerWeightT','loadSeq','moveTo','stow','status']
->>>>>>> b94042e2d527273bc7265fd45fc1e3ddb456db72
