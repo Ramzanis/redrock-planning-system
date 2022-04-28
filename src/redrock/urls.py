9ae8db6a44169227f9a35bd96713aecf3572a7cf
Ramzan <Ramzanismailovr@gmail.com>
2022-04-28 15:01:54 +0200

    Fetching from main branch
    Merging code from main to current branch.

--- src/redrock/urls.py
+++ src/redrock/urls.py
@@ -12,17 +12,18 @@ router.register('tes', views.SubtaskViewSet, basename='status')
 
 # URLConf
 urlpatterns = [
-    path('', views.displayindex, name='displayindex'),
-    path('order', views.displayorder, name='displayorder'),
-    path('order/add', views.add_operation, name='addoperation'),
-    path('order/update/<operationID>',
+     path('', views.displayindex, name='displayindex'),
+     path('order', views.displayorder, name='displayorder'),
+     path('order/add', views.add_operation, name='addoperation'),
+     path('order/update/<operationID>',
          views.update_operation, name='updateoperation'),
-    path('order/delete/<operationID>',
+     path('order/delete/<operationID>',
          views.delete_operation, name='deleteoperation'),
-    path('planning/', views.displaydata, name='displaydata'),
-    path('planning/add', views.add_subtask, name='addsubtask'),
-    path('planning/update/<subtaskID>',
+     path('planning/', views.displaydata, name='displaydata'),
+     path('planning/finisd', views.displayfinish, name='displayfinish'),
+     path('planning/add', views.add_subtask, name='addsubtask'),
+     path('planning/update/<subtaskID>',
          views.update_subtask, name='updatesubtask'),
-    path('planning/delete/<subtaskID>',
+     path('planning/delete/<subtaskID>',
          views.delete_subtask, name='deletesubtask')
 ]
