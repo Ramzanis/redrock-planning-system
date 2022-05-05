5cc305b1abfd8ab07ead2f3efac4222a0e8afbf6
MrTiberiu <39731777+MrTiberiu@users.noreply.github.com>
2022-05-05 15:23:16 +0300

    Import/Export
    Import/Export on everything

--- src/redrock/admin.py
+++ src/redrock/admin.py
@@ -1,28 +1,37 @@
 from django.contrib import admin, messages
 from django.db.models.aggregates import Count
 from . import models
-from .resources import SubtaskResource
+from .resources import *
 from import_export.admin import ImportExportModelAdmin
 
 @admin.register(models.Employee)
 class EmployeeAdmin(ImportExportModelAdmin):
     list_display = ['employeeID', 'firstName','lastName','address','zipCode','city']
     list_editable = ['firstName','lastName','address','zipCode','city']
+    resource_class = EmployeeResource
+    list_per_page = 10
 
 @admin.register(models.Status)
 class StatusAdmin(ImportExportModelAdmin):
     list_display = ['statusID', 'status']
     list_editable = ['status']
+    resource_class = StatusResource
+    list_per_page = 10
 
 @admin.register(models.MoveTo)
 class MoveToAdmin(ImportExportModelAdmin):
     list_display = ['moveToID', 'location']
     list_editable = ['location']
+    resource_class = MoveToResource
+    list_per_page = 10
 
 @admin.register(models.Stow)
 class StowAdmin(ImportExportModelAdmin):
     list_display = ['stowID', 'locationStow']
     list_editable = ['locationStow']
+    resource_class = StowResource
+    list_per_page = 10
+
 
 @admin.register(models.Operation)
 class OperationAdmin(ImportExportModelAdmin):
@@ -30,6 +39,7 @@ class OperationAdmin(ImportExportModelAdmin):
                     'dateRegistered', 'timeStart', 'timeFinish', 'status']
     list_editable = ['assignee', 'status']
     list_filter = ['status', 'dateRegistered']
+    resource_class = OperationResource
     list_per_page = 10
 
     def status(self, operation):
--- src/redrock/resources.py
+++ src/redrock/resources.py
@@ -1,8 +1,40 @@
 from import_export import resources
-from redrock.models import Subtask
+from redrock.models import Employee, MoveTo, Operation, Status, Stow, Subtask
 
 class SubtaskResource(resources.ModelResource):
     class Meta:
         model = Subtask
-        exclude = ('id')
+        import_id_fields = ['subtaskID']
         skip_unchanged = True
+
+class OperationResource(resources.ModelResource):
+    class Meta:
+        model = Operation
+        import_id_fields = ['operationID']
+        skip_unchanged = True
+
+class EmployeeResource(resources.ModelResource):
+    class Meta:
+        model = Employee
+        import_id_fields = ['employeeID']
+        skip_unchanged = True
+
+class StatusResource(resources.ModelResource):
+    class Meta:
+        model = Status
+        import_id_fields = ['statusID']
+        skip_unchanged = True
+
+class MoveToResource(resources.ModelResource):
+    class Meta:
+        model = MoveTo
+        import_id_fields = ['moveToID']
+        skip_unchanged = True
+
+class StowResource(resources.ModelResource):
+    class Meta:
+        model = Stow
+        import_id_fields = ['stowID']
+        skip_unchanged = True
+
+
